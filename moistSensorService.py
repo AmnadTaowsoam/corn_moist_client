1d42af3058711b703fd21429ba10db8599e1802d
visanup <visanuace@gmail.com>
2024-03-25 10:26:37 +0700

    Update API Port 8007

--- moistSensorService.py
+++ moistSensorService.py
@@ -3,8 +3,10 @@ import threading
 import time
 import configparser
 import logging
+#from paho.mqtt import client as mqtt_client
 import warnings
-from paho.mqtt import client as mqtt_client
+import requests
+import json
 
 warnings.filterwarnings('ignore')
 # Logging and Configuration
@@ -15,52 +17,42 @@ class MoistureSensor:
         self.config = configparser.ConfigParser()
         self.config.read(config_path)
         sensor_config = self.config['MoistureSensor']
-
+        
         self.host = sensor_config.get('host')
         self.port = sensor_config.getint('port')
         self.address = sensor_config.getint('address')
         self.num_registers = sensor_config.getint('num_registers')
-        self.sensorId = sensor_config.get('sensorId')
-
+        self.sensorid = sensor_config.get('sensorid')
+        
         self.client = ModbusClient(host=self.host, port=self.port, auto_open=True)
+        
+    def read_data(self):
+        return self.client.read_holding_registers(self.address, self.num_registers)
 
-        # MQTT Configuration
-        mqtt_config = self.config['MQTT']
-        self.mqtt_broker = mqtt_config.get('broker')
-        self.mqtt_port = mqtt_config.getint('port')
-        self.mqtt_topic = mqtt_config.get('topic')
-        self.mqtt_client_id = f'python-mqtt-{self.sensorId}'
-
-    def mqtt_connect(self):
-        def on_connect(client, userdata, flags, rc):
-            if rc == 0:
-                logging.info("Connected to MQTT Broker!")
-            else:
-                logging.error("Failed to connect, return code %d\n", rc)
-
-        self.mqtt_client = mqtt_client.Client(self.mqtt_client_id)
-        self.mqtt_client.on_connect = on_connect
-        self.mqtt_client.connect(self.mqtt_broker, self.mqtt_port)
-        self.mqtt_client.loop_start()
-
-    def read_data_and_publish(self):
-        try:
-            while True:
-                data = self.client.read_holding_registers(self.address, self.num_registers)
-                if data and data[6] == 1:
-                    self.mqtt_client.publish(self.mqtt_topic, str(data[0]))
-                elif data and data[6] == 0:
-                    break  # หยุดการส่งข้อมูล
-                time.sleep(1)  # รอ 1 วินาทีก่อนอ่านข้อมูลครั้งถัดไป
-        except Exception as e:
-            logging.error(f"Error reading data from moisture sensor: {e}")
-
-    def start(self):
-        self.mqtt_connect()
-        read_thread = threading.Thread(target=self.read_data_and_publish)
-        read_thread.daemon = True
-        read_thread.start()
+    def close_connection(self):
+        self.client.close()
 
 if __name__ == "__main__":
     sensor = MoistureSensor()
-    sensor.start()
+    moisture_percentages = []
+    try:
+        while True:
+            data = sensor.read_data()
+            if data:
+                print("Raw moisture sensor data:", data)            
+                moisture_percentages.extend(data)
+
+                #แปลงข้อมูลเป็น JSON string
+                json_data = json.dumps({"value":data})
+                #บันทึก JSON string ลงในไฟล์
+                with open("sensor_data.json", "w") as file:
+                    file.write(json_data)
+
+                response = requests.post("http://localhost:8007/moisture",data=json_data, headers={"Content-Type": "application/json"})
+            else:
+                print("Failed to read data from the moisture sensor")
+            time.sleep(1)
+    except KeyboardInterrupt:
+         print("Stopped reading data from the moisture sensor")
+    finally:
+        sensor.close_connection()
